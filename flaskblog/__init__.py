46871625782f8af9eb7e5cb985f4be6ffd304196
JefferyRoeder <jefferyroeder@gmail.com>
2020-02-18 14:03:33 -0600

    split routes and forms with Blueprints

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 import os
+from flaskblog import env
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "d53f305771bff1ab035b303277b03436"
@@ -16,8 +17,18 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORRT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_USERNAME'] = env.user
+app.config['MAIL_PASSWORD'] = env.password
 mail = Mail(app)
 
-from flaskblog import routes
+from flaskblog.users.routes import users
+
+app.register_blueprint(users)
+
+from flaskblog.posts.routes import posts
+
+app.register_blueprint(posts)
+
+from flaskblog.main.routes import main
+
+app.register_blueprint(main)
